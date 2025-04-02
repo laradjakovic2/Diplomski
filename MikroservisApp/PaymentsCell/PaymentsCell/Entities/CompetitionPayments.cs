b48a4da55cb22cd7fc06a47bdb0e5373e6ef92c0
laradjakovic229 <laradjakovic00@gmail.com>
2025-04-02 16:52:53 +0200

    com payments

---
+++ MikroservisApp/PaymentsCell/PaymentsCell/Entities/CompetitionPayments.cs
@@ -0,0 +1,13 @@
+﻿namespace PaymentsCell.Entities
+{
+    public class CompetitionPayments
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CompetitionId { get; set; }
+        public string UserEmail { get; set; }
+        public double Price { get; set; }
+        public double Tax { get; set; }
+        public double Total { get; set; }
+    }
+}
